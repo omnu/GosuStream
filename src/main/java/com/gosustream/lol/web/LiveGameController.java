9631d8a94d4508cc58d21cbc4745bafc55139f97
omnu <syy0320@gmail.com>
2014-09-09 01:08:25 -0700

    Return game id for op.gg game search instead of link

--- src/main/java/com/gosustream/lol/web/LiveGameController.java
+++ src/main/java/com/gosustream/lol/web/LiveGameController.java
@@ -152,6 +152,7 @@ public class LiveGameController {
                 }
             }
         }
+         gameLink = gameLink.substring(gameLink.indexOf("id=") + 3);
         return gameLink;
     }
 
